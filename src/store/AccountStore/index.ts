d161e087884832b3af960e897dda19490335d53f
MinwookJo <alsdnr0537@naver.com>
2019-07-06 16:46:19 +0900

    MJ: Remove AccountStore

--- src/store/AccountStore/index.ts
+++
@@ -1,9 +0,0 @@
-import {observable, action} from 'mobx'
-
-class AccountStore {
-    @observable userId: string = '';
-    @observable password: string = ''; 
-    @observable deviceToken: string = ''; 
-}
-
-export default AccountStore;
