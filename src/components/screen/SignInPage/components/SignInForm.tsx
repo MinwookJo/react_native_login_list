bf70bbd399058bb4c9171838942bd69c439e1273
MinwookJo <alsdnr0537@naver.com>
2019-07-06 12:04:26 +0900

    MJ: Add State used in form

--- src/components/screen/SignInPage/components/SignInForm.tsx
+++ src/components/screen/SignInPage/components/SignInForm.tsx
@@ -1,24 +1,47 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 import styles from "../styles";
 import TitleTextInput from "../../../molecule/TitleTextInput";
 
-class SignInForm extends React.Component {
+type State = {
+    userId: string,
+    password: string
+}
 
-    _onChange = (value: string) => {
-        console.log(value);
+const initialState: State = {
+    userId: '',
+    password: ''
+}
+
+class SignInForm extends React.Component {
+    state = initialState;
+
+    private onChangeUserId = (value: string) => {
+        this.setState({userId: value});
+    }
+
+    private onChangePassword = (value: string) => {
+        this.setState({password: value});
     }
 
     render() {
         return(
             <View style={{flex: 1, flexDirection: 'column', alignItems: 'center'}}>
-                <TitleTextInput headerText={'ID'} onChangeText={this._onChange}/>
-                <TitleTextInput headerText={'Password'} onChangeText={this._onChange}/>
+                <TitleTextInput headerText={'ID'} onChangeText={this.onChangeUserId}/>
+                <TitleTextInput headerText={'Password'} onChangeText={this.onChangePassword}/>
 
                 <View style={{flexDirection: 'row'}}>
                     <View style={styles.checkBox}/>
                     <Text style={styles.checkBoxSubtitle}>로그인 상태 유지</Text>
                 </View>
+                
+                <TouchableOpacity>
+                    <Text>로그인</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity>
+                    <Text>비밀번호 찾기</Text>
+                </TouchableOpacity>
             </View>
         );
     }
--- src/components/screen/SignInPage/styles.ts
+++ src/components/screen/SignInPage/styles.ts
@@ -1,21 +1,5 @@
 import {StyleSheet, ImageStyle, ViewStyle, TextStyle} from 'react-native';
 
-const loginInputField = {
-    width: 296,
-    height: 48,
-    borderWidth: 1,
-    borderColor: '#2e2e2e',
-    borderRadius: 4
-}
-
-const loginInputFieldSubtitle: TextStyle = {
-    fontSize: 14,
-    fontWeight: 'normal',
-    lineHeight: 14,
-    color: '#999999',
-    marginBottom: 6
-}
-
 const checkBox: ViewStyle = {
     width: 18,
     height: 18,
@@ -32,12 +16,11 @@ const checkBoxSubtitle: TextStyle = {
 const siginInIcon: ImageStyle & ViewStyle = {
     width: 120,
     height: 120,
-    backgroundColor: 'grey'
+    backgroundColor: 'grey',
+    marginTop: 100
 }
 
 export default StyleSheet.create({
-    loginInputField,
-    loginInputFieldSubtitle,
     siginInIcon,
     checkBox,
     checkBoxSubtitle,
