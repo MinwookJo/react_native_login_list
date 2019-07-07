3c4b1c2f5bb4ba87008d86b69fc6941e71f7392f
MinwookJo <alsdnr0537@naver.com>
2019-07-07 23:31:33 +0900

    MJ: Add getOsType function in device util

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,6 @@
 export const BASE_API_URL = 'https://mcricwiojwfb.cleancitynetworks.com/';
 
+// response data 로 받아올 API Response 기본 타입
 export type ApiResponseType<T> = {
     statusCode?: number,
     error?: string,
--- src/utils/device.ts
+++ src/utils/device.ts
@@ -1,4 +1,17 @@
+import { Platform } from "react-native";
+
 export enum DEVICE_TYPE {
     Ios = 'ios',
-    Android = 'android'
+    Android = 'android',
+    Undefine = 'undefine'
+}
+
+export const getOS = (): DEVICE_TYPE => {
+    if(Platform.OS === 'ios') {
+        return DEVICE_TYPE.Ios
+    } else if (Platform.OS === 'android') {
+        return DEVICE_TYPE.Android
+    } else {
+        return DEVICE_TYPE.Undefine
+    }
 }
